4a9067b2a654b7609a6b3b74d42520f1e1d9ce6a
Willi27 <coach72305@gmail.com>
2020-10-28 12:11:04 +0200

    Remove files

--- Lesson 3/Les3.13.py
+++
@@ -1,8 +0,0 @@
-H = int(input()) # Часы
-M = int(input()) # Минуты
-S = int(input()) # Секунды
-
-g = (60*60*12)/360 # скорость движения на 1 градус в секугдах
-currentTime = H*3600 + M*60 + S
-degrees = currentTime/120 # градусы
-print(degrees)
--- Lesson 3/Les3.4.py
+++
@@ -1,5 +0,0 @@
-import math
-
-number = float(input())
-fin2 = (number - math.floor(number))*10
-print(math.floor(fin2))
